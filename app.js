f429f3e2bd6c6001dd89fc6902a1b489028b5207
Abdullah AlJasser <amj.jasser@gmail.com>
2021-10-23 18:53:48 -0400

    initial

---
+++ app.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const errorHandler = require("./middleware/error");
+//const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+const index = require("./routes/index");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+// Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", index);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+);
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
