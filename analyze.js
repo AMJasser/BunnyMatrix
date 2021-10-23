682c4a1e8238a57f35dbc592a604d586af8e4cad
Faris Durrani <afarisdurrani@gmail.com>
2021-10-23 12:45:50 -0400

    D3 Init

--- analyze.js
+++ analyze.js
@@ -1,35 +1,5 @@
-var revenue_CI = [13700, 18100]
-var profit_CI = [1060, 1620]
-var employees_CI = [28900, 40300]
-var market_cap_CI = [26300, 42700]
+console.log("heh")
 
-function analyze() {
-    var revenue = document.getElementById("revenue").value;
-    var profit = document.getElementById("profit").value;
-    var employees = document.getElementById("employees").value;
-    var market_cap = document.getElementById("market_cap").value;
-
-    var success = 0;
-    if (revenue != "" && withinRange(revenue, revenue_CI)) {
-        success += 25;
-    }
-    if (revenue != "" && withinRange(profit, profit_CI)) {
-        success += 25;
-    }
-    if (withinRange(employees, employees_CI)) {
-        success += 25;
-    }
-    if (withinRange(market_cap, market_cap_CI)) {
-        success += 25;
-    }
-
-    console.log("bob");
-}
-
-function withinRange(value, list) {
-    if (value >= list[0] && value <= list[1]) {
-        return true;
-    } else {
-        return false;
-    }
-}
+d3.csv("./datasets/walmart/full_walmart_2010-2013.csv", function(data) {
+    console.log(data);
+});
